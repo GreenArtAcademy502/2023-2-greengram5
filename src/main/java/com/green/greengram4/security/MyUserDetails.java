a0a17f1e84d0a71196cbba2a55be33fb3cbd540f
DoheumPark <pirbak@hanmail.net>
2024-02-08 13:20:01 +0900

    24-02-08 Social Login 작업중(2)

--- src/main/java/com/green/greengram4/security/MyUserDetails.java
+++ src/main/java/com/green/greengram4/security/MyUserDetails.java
@@ -4,16 +4,19 @@ import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Data
 @Builder
-public class MyUserDetails implements UserDetails {
+public class MyUserDetails implements UserDetails, OAuth2User {
 
     private MyPrincipal myPrincipal;
+    private Map<String, Object> attributes;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -49,5 +52,10 @@ public class MyUserDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return false;
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
--- src/main/java/com/green/greengram4/security/oauth2/userinfo/CustomeOAuth2UserService.java
+++ src/main/java/com/green/greengram4/security/oauth2/userinfo/CustomeOAuth2UserService.java
@@ -1,15 +1,18 @@
 package com.green.greengram4.security.oauth2.userinfo;
 
+import com.green.greengram4.security.oauth2.SocialProviderType;
 import com.green.greengram4.user.UserMapper;
+import com.green.greengram4.user.model.UserEntity;
+import com.green.greengram4.user.model.UserSelDto;
+import com.green.greengram4.user.model.UserSignupProcDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-
-import javax.naming.AuthenticationException;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +30,42 @@ public class CustomeOAuth2UserService extends DefaultOAuth2UserService {
         } catch (Exception e) {
             throw new InternalAuthenticationServiceException(e.getMessage(), e.getCause());
         }
+    }
+
+    private OAuth2User process(OAuth2UserRequest userRequest, OAuth2User user) {
+        SocialProviderType socialProviderType
+                    = SocialProviderType.valueOf(userRequest.getClientRegistration()
+                                                            .getRegistrationId()
+                                                            .toUpperCase()
+                    );
+        user.getAttributes();
+
+        OAuth2UserInfo oAuth2UserInfo = factory.getOAuth2UserInfo(socialProviderType, user.getAttributes());
+        UserSelDto dto = UserSelDto.builder()
+                .providerType(socialProviderType.name())
+                .uid(oAuth2UserInfo.getId())
+                .build();
+        UserEntity savedUser = mapper.selUser(dto);
+        if(savedUser == null) { //회원가입 처리
+            savedUser = signupUser(oAuth2UserInfo, socialProviderType);
+        }
+
+        return null;
+    }
+
+    private UserEntity signupUser(OAuth2UserInfo oAuth2UserInfo, SocialProviderType socialProviderType) {
+        UserSignupProcDto dto = new UserSignupProcDto();
+        dto.setProviderType(socialProviderType.name());
+        dto.setUid(oAuth2UserInfo.getId());
+        dto.setUpw("social");
+        dto.setNm(oAuth2UserInfo.getName());
+        dto.setPic(oAuth2UserInfo.getImageUrl());
+        dto.setRole("USER");
+        int result = mapper.insUser(dto);
+
+        UserEntity entity = new UserEntity();
+        entity.setIuser(dto.getIuser());
+        entity.setRole(dto.getRole());
+        return entity;
     }
 }
