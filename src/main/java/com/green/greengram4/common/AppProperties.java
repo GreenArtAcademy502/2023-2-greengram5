59d9b3017164474f359eebd84d948c4f6059200c
DoheumPark <pirbak@hanmail.net>
2024-02-13 12:59:07 +0900

    24-02-13 Social Login 작업중(5)

--- src/main/java/com/green/greengram4/common/AppProperties.java
+++ src/main/java/com/green/greengram4/common/AppProperties.java
@@ -4,11 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @ConfigurationProperties(prefix = "app")
-public class AppProperties {
+public final class AppProperties {
 
     private final Jwt jwt = new Jwt();
+    private final Oauth2 oauth2 = new Oauth2();
 
     @Getter
     @Setter
@@ -25,5 +29,10 @@ public class AppProperties {
             this.refreshTokenCookieMaxAge =
                     (int) (refreshTokenExpiry * 0.001);
         }
+    }
+
+    @Getter
+    public static final class Oauth2 {
+        private List<String> authorizedRedirectUris = new ArrayList();
     }
 }
