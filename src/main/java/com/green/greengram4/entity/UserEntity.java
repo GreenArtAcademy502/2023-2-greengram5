28df58517426c731a4ed303019e8b03cddf848d8
DoheumPark <pirbak@hanmail.net>
2024-02-14 12:00:46 +0900

    24-02-14 JPA (2) t_user 완성

--- src/main/java/com/green/greengram4/entity/UserEntity.java
+++ src/main/java/com/green/greengram4/entity/UserEntity.java
@@ -1,21 +1,43 @@
 package com.green.greengram4.entity;
 
 import com.green.greengram4.common.ProviderTypeEnum;
+import com.green.greengram4.common.RoleEnum;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
 
 @Data
 @Entity
 @Table(name = "t_user")
 public class UserEntity extends BaseEntity {
     @Id
+    @Column(columnDefinition = "BIGINT UNSIGNED")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long iuser;
 
     @Column(length = 10, name = "provider_type", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    @ColumnDefault("'LOCAL'")
     private ProviderTypeEnum providerType;
 
     @Column(length = 100, nullable = false)
     private String uid;
+
+    @Column(length = 300, nullable = false)
+    private String upw;
+
+    @Column(length = 25, nullable = false)
+    private String nm;
+
+    @Column(length = 2100)
+    private String pic;
+
+    @Column(length = 2100, name = "firebase_token")
+    private String firebaseToken;
+
+    @Column(length = 10, nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    @ColumnDefault("'USER'")
+    private RoleEnum role;
 
 }
